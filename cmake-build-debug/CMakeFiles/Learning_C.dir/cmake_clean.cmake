ec9b80f027ae04f0137951f78c2ac547d6caa89a
Spokoziomek123 <fioooli19@gmail.com>
2023-11-26 16:59:12 +0100

    Making changes in test file

---
+++ cmake-build-debug/CMakeFiles/Learning_C.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Learning_C.dir/Notes/test.c.obj"
+  "CMakeFiles/Learning_C.dir/Notes/test.c.obj.d"
+  "Learning_C.exe"
+  "Learning_C.exe.manifest"
+  "Learning_C.pdb"
+  "libLearning_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Learning_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
